c0509e9b9e46e49862b24f27a61caaeba54faf3f
Arshit rupani <125478383+arshitrupanii@users.noreply.github.com>
2024-11-08 14:55:10 +0530

    macbook

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const current_song = new Audio();
+// this is footer play/pause button
+let play_pause_footer = document.getElementsByClassName('playbar')[0].getElementsByClassName('play_pause')[0].getElementsByTagName('img')[0];
+
+
+// getting all songs from the server location
 async function getsongs() {
     let song_location = await fetch("http://127.0.0.1:5500/songs/")
     let responce = await song_location.text()
@@ -15,8 +21,17 @@ async function getsongs() {
     return songs;
 }
 
+// for play songs
+const playmusic = (path) => {
+    current_song.src =`/songs/${path}`
+    current_song.play()
+}
+
+
+// this is the main function
 async function main() {
     let song = await getsongs()
+    let play = document.getElementById('play');
 
     let songlist = document.querySelector('.songlist').getElementsByTagName('ol')[0];
 
@@ -41,13 +56,51 @@ async function main() {
         `;
     }
 
-    let audio = new Audio(song[2])
-    // await audio.play()
+    // audio.addEventListener("loadeddata", () => {
+    //     let duration = audio.duration;
+    //     console.log(duration);
+    // })    
 
-    audio.addEventListener("loadeddata", () => {
-        let duration = audio.duration;
-        console.log(duration);
+    // play the song when click on song name
+    Array.from(document.querySelector('.songlist').getElementsByTagName('li')).forEach((e) => {
+        let song_name = e.getElementsByClassName('info')[0].getElementsByClassName('song_name')[0].innerText;
+    
+        let play_pause_sidebar_btn = e.getElementsByClassName('flex align-item gap')[0].getElementsByTagName('img')[0];
+        
+        // attaching evenlistener to all songs
+        e.addEventListener('click', () => {
+            if(current_song.paused){
+                play_pause_sidebar_btn.src = 'https://img.icons8.com/?size=100&id=36268&format=png&color=FFFFFF'
+                play_pause_footer.src = 'svg/play.svg'
+                playmusic(song_name)
+            }
+
+            else{
+                play_pause_sidebar_btn.src = 'https://img.icons8.com/?size=100&id=36067&format=png&color=FFFFFF'
+                play_pause_footer.src = 'svg/pause.svg'
+                current_song.pause()
+            }
+        })
+
+        // attaching even listener to play pause song
+        
     })
+
+    
+    play_pause_footer.addEventListener('click', () => {
+        if(current_song.paused){
+            play_pause_sidebar_btn.src = 'https://img.icons8.com/?size=100&id=36268&format=png&color=FFFFFF'
+            play_pause_footer.src = 'svg/play.svg'
+            current_song.play()
+        }
+
+        else{
+            play_pause_sidebar_btn.src = 'https://img.icons8.com/?size=100&id=36067&format=png&color=FFFFFF'
+            play_pause_footer.src = 'svg/pause.svg'
+            current_song.pause()
+        }
+    })
+
 }
 
 main()
